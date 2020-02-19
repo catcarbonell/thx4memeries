3c36fd3aeb08e210501c47b6a03bfb4bb50c1d14
catcarbonell <hello@catcarbn.com>
2020-02-19 10:52:56 -0800

    Added a randoQuote gen for the search bar lol.

---
+++ js/app.js
@@ -0,0 +1,28 @@
+console.log('oh heck.');
+/* 
+-- FRONT-END NOTES --
+// x  extra idea: generate random value quote for the main search
+// on-load: search bar in the center-center
+// after: search bar goes on top, upper-most z-index, opacity goes 0.8-0.9 when scrolling
+// upon hover, goes back to opacity: 1 
+// gifs on-scroll: fadeIn
+
+-- BACK-END NOTES --
+// jQuery Ajax call
+*/
+
+const randoQuoteLib = [
+    "Going my way?",
+    "Hello. Is it me you're lookin for?",
+    "Khajit has wares if u have coin.",
+    "One more time~ Search for the memeries",
+    "A/S/L?"
+];
+
+let randoQuoteGen = () => {
+    const randoMath = Math.random();
+    let i = randoQuoteLib.length;
+    return randoQuoteLib[Math.floor(i * randoMath)];
+}
+
+$('input[type=text]#main-input').val(randoQuoteGen());
