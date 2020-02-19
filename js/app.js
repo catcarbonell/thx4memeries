5f4f5ca63277610b9ad07b90ac2004c6e908d4a2
catcarbonell <hello@catcarbn.com>
2020-02-19 13:16:01 -0800

    Aligning stuff is hard lol.

--- js/app.js
+++ js/app.js
@@ -1,8 +1,8 @@
 console.log('oh heck.');
 /* 
 -- FRONT-END NOTES --
-// x  extra idea: generate random value quote for the main search
-// on-load: search bar in the center-center
+x //   extra idea: generate random value quote for the main search
+x // on-load: search bar in the center-center
 // after: search bar goes on top, upper-most z-index, opacity goes 0.8-0.9 when scrolling
 // upon hover, goes back to opacity: 1 
 // gifs on-scroll: fadeIn
@@ -11,18 +11,19 @@ console.log('oh heck.');
 // jQuery Ajax call
 */
 
+/* RANDOQUOTE INPUT */
 const randoQuoteLib = [
     "Going my way?",
-    "Hello. Is it me you're lookin for?",
+    "Hello. Is it me you're looking for?",
     "Khajit has wares if u have coin.",
     "One more time~ Search for the memeries",
     "A/S/L?"
 ];
-
 let randoQuoteGen = () => {
     const randoMath = Math.random();
     let i = randoQuoteLib.length;
     return randoQuoteLib[Math.floor(i * randoMath)];
 }
+$('input[type=text]#main-input').val("🔍 " + randoQuoteGen());
 
-$('input[type=text]#main-input').val(randoQuoteGen());
+/* */
