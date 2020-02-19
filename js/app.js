708e19817e5ee741be411010b4bcd4c7aff3d3a2
catcarbonell <hello@catcarbn.com>
2020-02-19 15:52:40 -0800

    Code formatting

--- js/app.js
+++ js/app.js
@@ -1,92 +1,75 @@
 $(document).ready(function(){
-console.log('oh heck.');
-// GIPHY API URL 
-const giphyURL = ' https://api.giphy.com/v1/gifs/search?api_key=P3e3lyyGy6pyTA8JjmwmitoJ0YCQe2PF&q=';
+    console.log('oh heck.');
+    // GIPHY API URL 
+    const giphyURL = ' https://api.giphy.com/v1/gifs/search?api_key=P3e3lyyGy6pyTA8JjmwmitoJ0YCQe2PF&q=';
 
-/* 
--- FRONT-END NOTES --
-x //   extra idea: generate random value quote for the main search
-x // on-load: search bar in the center-center
-// after: search bar goes on top, upper-most z-index, opacity goes 0.8-0.9 when scrolling
-// upon hover, goes back to opacity: 1 
-// gifs on-scroll: fadeIn
+    /* RANDOQUOTE INPUT */
+    const randoQuoteLib = [
+        "One more time~ Search for the memeries",
+        "Going my way?",
+        "Hello. Is it me you're looking for?",
+        "Khajit has wares if u have coin.",
+        "One more time~ Search for the memeries",
+        "A/S/L?",
+        "=^.^=",
+        "Hey.",
+        "One more time~ Search for the memeries",
+    ];
 
--- BACK-END NOTES --
-// jQuery Ajax call
-*/
-
-/* RANDOQUOTE INPUT */
-const randoQuoteLib = [
-    "Going my way?",
-    "Hello. Is it me you're looking for?",
-    "Khajit has wares if u have coin.",
-    "One more time~ Search for the memeries",
-    "A/S/L?"
-];
-
-let randoQuoteGen = () => {
-    const randoMath = Math.random();
-    let i = randoQuoteLib.length;
-    return randoQuoteLib[Math.floor(i * randoMath)];
-}
-$('input[type=text]#main-input').val("🔍 " + randoQuoteGen());
+    let randoQuoteGen = () => {
+        const randoMath = Math.random();
+        let i = randoQuoteLib.length;
+        return randoQuoteLib[Math.floor(i * randoMath)];
+    }
+    $('input[type=text]#main-input').val("🔍 " + randoQuoteGen());
 
 /* AJAXin */ 
 
 
-const form = document.querySelector('form');
+    const form = document.querySelector('form');
 
-// *** FUNCTIONS *** //
-function handleSearchSubmit(event) {
-    event.preventDefault();
-    const gifSearchInput = document.querySelector('#main-input');
-    document.querySelector('#gif-gallery').innerHTML = '';
-   
-    $.ajax(
-        {
-        method: 'GET',
-        url: giphyURL + gifSearchInput.value + "&offset=0&rating=G&lang=en",
-        success: function (response){ 
-            $('#clear-search').show();
-            for(i in response.data){
-                //$(".gif-item").slice(0, 4).fadeIn().show();
-                $('#gif-gallery').append( `
-                <div class="gif-item">
-                <img src=" ${response.data[i].images.downsized.url}" alt="TAGS" />
-                <p>source:</p>
-                <textarea>${response.data[i].images.downsized.url}</textarea>
-                </div>
-                `);
-            }
-          /*  $("#loadMore").show().on("click", function(e){
-                e.preventDefault();
-                $(".content:hidden").slice(0, 4).fadeIn();
-                if($(".content:hidden").length <= 0){
-                    $("#loadMore").hide();
-                    $("#top-link").show();
+    function handleSearchSubmit(event) {
+        event.preventDefault();
+        const gifSearchInput = document.querySelector('#main-input');
+        document.querySelector('#gif-gallery').innerHTML = '';
+    
+        $.ajax(
+            {
+            method: 'GET',
+            url: giphyURL + gifSearchInput.value + "&offset=0&rating=G&lang=en",
+            success: function (response){ 
+                $('#clear-search').show();
+                for(i in response.data){
+                    //$(".gif-item").slice(0, 4).fadeIn().show();
+                    $('#gif-gallery').append( `
+                    <div class="gif-item">
+                    <img src=" ${response.data[i].images.downsized.url}" alt="TAGS" />
+                    <p>source:</p>
+                    <textarea>${response.data[i].images.downsized.url}</textarea>
+                    </div>
+                    `);
                 }
-            });*/
-        },
-        error: function(error){
-            console.log(error);
-            console.log('oh no.')
-            $('div').append(`<h2>Whoops. What happened??</h2>`);
-        },
-    });
-    // Search reset!
-     gifSearchInput.value='';
-     $("#top-link").hide();
-}
+            },
+            error: function(error){
+                console.log(error);
+                console.log('oh no.')
+                $('div').append(`<h2>Whoops. What happened??</h2>`);
+            },
+        });
+        // Search reset!
+        gifSearchInput.value='';
+        $("#top-link").hide();
+    }
 
-// *** EVENT LISTENERS *** //
-form.addEventListener('submit', handleSearchSubmit); 
+    // *** EVENT LISTENERS *** //
+    form.addEventListener('submit', handleSearchSubmit); 
 
-$('#clear-search').on('click', function(e){
-    e.preventDefault();
-    $('.gif-gallery').html('<section id="gif-gallery"></section>');
-    $("#top-link").hide();
-    $("#loadMore").hide();
-    $("#clear-search").hide();
+    $('#clear-search').on('click', function(e){
+        e.preventDefault();
+        $('.gif-gallery').html('<section id="gif-gallery"></section>');
+        $("#top-link").hide();
+        $("#loadMore").hide();
+        $("#clear-search").hide();
 })
 
 
